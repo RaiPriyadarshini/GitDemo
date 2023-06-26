3391174efdbeafd46d5492b069080940a4143794
raipriyadarshini <priyaasingh2209@gmail.com>
2023-06-27 03:11:49 +0530

    This is commit from Assian time Zone

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -2,6 +2,14 @@
 public class Test {
 	//square bracket in json is called as array,array is collection of element,
 	//first element is treated as 0th index
-	
+ public static void main(String[] args) throws AWTException, InterruptedException, IOException {
+        
+       System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"//chromedriver.exe");
+       WebDriver driver = new ChromeDriver();
+       driver.manage().window().maximize();
+       driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+       driver.get("https://www.google.com");
+
+	}
 
 }
